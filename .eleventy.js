a0182035401848600d481c517c36d68bb156dff5
Kay <102677416+kayhardam@users.noreply.github.com>
2023-11-13 22:57:12 +0100

    added path

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/assets/blog');
+    eleventyConfig.addPassthroughCopy('./src/admin/config.yml');
 
     eleventyConfig.addFilter('postDate', (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
